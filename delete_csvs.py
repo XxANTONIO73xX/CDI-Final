6d892ac27a2e20b15f0b83f5e92cbca4947cee21
Ariana Espinoza <60475171+XxANTONIO73xX@users.noreply.github.com>
2023-11-22 07:49:08 -0800

    Final version

--- delete_csvs.py
+++
@@ -1,8 +0,0 @@
-import os
-from os import walk
-actualPath = os.getcwd()
-w = walk(actualPath+"/data")
-for (dirpath, dirnames, filenames) in w:
-    for file in filenames:
-        if file[-4:] == ".csv":
-            os.remove(dirpath+"/"+file)
